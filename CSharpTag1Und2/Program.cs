3e6b2eabdf7409d9fd381c985a47c819b13f08b6
David Tielke <david.tielke@hotmail.de>
2023-06-12 12:01:05 +0200

    Üb ung 2

--- CSharpTag1Und2/Program.cs
+++ CSharpTag1Und2/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DelegatesUndEvents;
+
+public class Program
+{
+    // Übung 1:
+    // - Fuechtigkeitssensor
+    // - Methode Messe
+    // - Delegaten-Feld NeuerMesswert
+    // - Mit eigenem Delegaten
+    // Wert in Consolenanwendung ausgeben
+
+    public static void Main()
+    {
+        var del1 = new Action(MyHandler);
+        del1();
+
+        var del2 = new Action<double>(MyHandler);
+        del2(1.23);
+
+        var del3 = new Func<int, int, long>(Add);
+        var sum = del3(1, 2);
+
+        var del4 = new Func<string, bool>(IsValid);
+        var isValid = del4("David");
+    }
+
+    public static bool IsValid(string name)
+    {
+        return name.Length > 2;
+    }
+
+    public static long Add(int zahl1, int zahl2)
+    {
+        return zahl1 + zahl2;
+    }
+
+    public static void MyHandler()
+    {
+        Console.WriteLine("Mein eigener Quellcode");
+    }
+
+    public static void MyHandler(double wert)
+    {
+        Console.WriteLine($"Mein eigener Quellcode: {wert}");
+    }
+}
+
+public delegate void MyClickHandler();
+
+// MyNewWindowsForms.dll
+internal class Button
+{
+    public MyClickHandler _clickHandler;
+
+    public void Click()
+    {
+        // ....
+        _clickHandler();
+        // ....
+    }
+}
---
+++ Übung1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Übung1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var sensor = new Sensor();
+
+        sensor._newMesswert += LogMesswert;
+        sensor._newMesswert += GebeMesswertAus;
+        sensor._newMesswert -= LogMesswert;
+
+
+        sensor.Messe();
+    }
+
+    private static void LogMesswert(double wert)
+    {
+        File.WriteAllText("values.txt", wert + "\n");
+    }
+
+    private static void GebeMesswertAus(double wert)
+    {
+        Console.WriteLine($"Neuer Messwert {wert}");
+    }
+}
+
+public delegate void NewMesswertHandler(double wert);
+
+internal class Sensor
+{
+    public NewMesswertHandler _newMesswert;
+
+    public void Messe()
+    {
+        _newMesswert(1.23d);
+    }
+}
